721fbb63f9a67873adb24918ce52cd5206ebd37d
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-31 17:44:11 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "The_Lighthouse" `
+  -ModName "The Lighthouse" `
+  -ModFolder "TheLighthouse" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/the-lighthouse/TheLighthouse-Original.zip" `
+  -ModBaseFilesUrlHash "17c67816ab84896d1ddb41b0399670003f537dd80dd992b4ead68e83b6821f4d" `
+  -ModReadmePath "readme.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
